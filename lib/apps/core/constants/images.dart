b863c9a067f912ee2c8c2486e1d757d6a0f96b7b
cahyo40 <mdcnugroho@gmail.com>
2025-10-08 22:02:54 +0700

    feat(constants): add dedicated file for image asset constants

---
+++ lib/apps/core/constants/images.dart
@@ -0,0 +1,9 @@
+class Images {
+  Images._();
+
+  static String _call(String image) => "assets/image/$image";
+
+  static String onboard1 = _call("onboard1.png");
+  static String onboard2 = _call("onboard2.png");
+  static String onboard3 = _call("onboard3.png");
+}

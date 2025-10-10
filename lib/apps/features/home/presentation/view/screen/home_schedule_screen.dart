51fcb41af509355352c94301829b29d0b8dea6fc
cahyo40 <mdcnugroho@gmail.com>
2025-10-10 22:32:59 +0700

    feat(home-schedule): add today’s schedule list

--- lib/apps/features/home/presentation/view/screen/home_schedule_screen.dart
+++ lib/apps/features/home/presentation/view/screen/home_schedule_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:twogass/apps/data/model/schedule_model.dart';
+import 'package:twogass/apps/widget/card_schedule_widget.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../../controller/home_controller.dart';
 
@@ -8,8 +11,65 @@ class HomeScheduleScreen extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Text('HomeScheduleScreen is working'),
+    return ListView(
+      shrinkWrap: true,
+      physics: AlwaysScrollableScrollPhysics(),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            YoText.titleMedium("Jadwal hari ini (3)"),
+            TextButton(
+              onPressed: () {},
+              child: YoText.bodyMedium(
+                "Lihat semua jadwal",
+                color: context.primaryColor,
+              ),
+            ),
+          ],
+        ),
+        ListView(
+          shrinkWrap: true,
+          physics: AlwaysScrollableScrollPhysics(),
+          children: [
+            CardScheduleWidget(
+              model: ScheduleModel(
+                id: "id",
+                uid: "uid",
+                type: "private",
+                date: DateTime.now().subtract(Duration(hours: 30)),
+                creatdAt: DateTime.now(),
+                title: "Testing Pribadi",
+                description: "lorem ipsum buat pribadi",
+              ),
+            ),
+            CardScheduleWidget(
+              model: ScheduleModel(
+                id: "id",
+                uid: "uid",
+                type: "private",
+                date: DateTime.now().subtract(Duration(hours: 50)),
+                creatdAt: DateTime.now(),
+                title: "Testing Pribadi",
+                description: "lorem ipsum buat pribadi",
+              ),
+              showDate: false,
+            ),
+            CardScheduleWidget(
+              model: ScheduleModel(
+                id: "id",
+                uid: "uid",
+                type: "org",
+                orgId: YoIdGenerator.uuid(),
+                date: DateTime.now(),
+                creatdAt: DateTime.now(),
+                title: "Testing Organisasi",
+                description: "Lorem ipsum buat organisasi",
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }

0196fba46cc9b1333dbdc0a269f9a41485520b15
cahyo40 <mdcnugroho@gmail.com>
2025-10-10 22:32:36 +0700

    feat(home-header): add dynamic greeting, user name, email, and avatar display

--- lib/apps/features/home/presentation/view/screen/home_header_screen.dart
+++ lib/apps/features/home/presentation/view/screen/home_header_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:twogass/apps/controller/auth_controller.dart';
+import 'package:twogass/apps/core/helpers/date_helpers.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../../controller/home_controller.dart';
 
@@ -8,8 +11,33 @@ class HomeHeaderScreen extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Text('HomeHeaderScreen is working'),
+    final user = Get.find<AuthController>();
+    return Padding(
+      padding: YoPadding.onlyBottom16,
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                YoText.titleLarge(YoDateFormatter().greeting(context)),
+                YoText.titleMedium(
+                  user.name,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: true,
+                ),
+                YoText.bodySmall(user.email),
+              ],
+            ),
+          ),
+          CircleAvatar(
+            radius: 25,
+            backgroundImage: NetworkImage(user.photoUrl),
+            backgroundColor: context.primaryColor,
+          ),
+        ],
+      ),
     );
   }
 }

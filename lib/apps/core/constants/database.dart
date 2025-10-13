ec56307f242cc04e639aa86ee61ae4941ac091cb
cahyo40 <mdcnugroho@gmail.com>
2025-10-13 08:12:28 +0700

    feat(constants): add DatabaseConst for collection/table names

---
+++ lib/apps/core/constants/database.dart
@@ -0,0 +1,10 @@
+class DatabaseConst {
+  String user = "user";
+  String org = "organization";
+  String project = "project";
+  String task = "task";
+  String schedule = "schedule";
+  String notification = "notification";
+  String voting = "voting";
+  // static String user = "user";
+}

b848a108d347bb256e18294eff5ef9da6be5deb6
cahyo40 <mdcnugroho@gmail.com>
2025-10-13 08:12:08 +0700

    feat(initial-binding): register SembastDatabase service and Login datasources/repository

--- lib/apps/bindings/initial_bindings.dart
+++ lib/apps/bindings/initial_bindings.dart
@@ -1,11 +1,37 @@
 import 'package:get/get.dart';
-import 'package:twogass/apps/controller/locale_controller.dart';
-import 'package:twogass/apps/controller/theme_controller.dart';
+import 'package:twogass/apps/core/services/sembast.dart';
+import 'package:twogass/apps/features/login/data/datasource/login_network_datasource.dart';
+import 'package:twogass/apps/features/login/data/datasource/login_offline_datasource.dart';
+import 'package:twogass/apps/features/login/data/repositories/login_repository_impl.dart';
+import 'package:twogass/apps/features/login/domain/repositories/login_repository.dart';
 
 class InitialBindings implements Bindings {
   @override
   void dependencies() {
-    Get.put(ThemeController(), permanent: true);
-    Get.put(LocaleController(), permanent: true);
+    //service
+    Get.put<SembastDatabase>(SembastDatabase());
+    // Get.put();
+
+    // repository
+    // LOGIN
+    Get.lazyPut<LoginRepository>(
+      () => LoginRepositoryImpl(
+        Get.find<LoginNetworkDatasource>(),
+        Get.find<LoginOfflineDatasource>(),
+      ),
+      fenix: true,
+    );
+
+    // Datasource
+    // LOGIN OFFLINE
+    Get.lazyPut<LoginOfflineDatasource>(
+      () => LoginOfflineDatasource(Get.find<SembastDatabase>()),
+      fenix: true,
+    );
+    // LOGIN ONLINE
+    Get.lazyPut<LoginNetworkDatasource>(
+      () => LoginNetworkDatasource(),
+      fenix: true,
+    );
   }
 }

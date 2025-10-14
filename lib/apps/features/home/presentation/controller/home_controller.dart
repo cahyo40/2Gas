504d5341fc2eca0fbb22dcfeb31a5be92821ac1b
cahyo40 <mdcnugroho@gmail.com>
2025-10-14 07:18:07 +0700

    feat(home): add addOrganization, joinOrganization, and detailOrganization methods

--- lib/apps/features/home/presentation/controller/home_controller.dart
+++ lib/apps/features/home/presentation/controller/home_controller.dart
@@ -1,6 +1,20 @@
 import 'package:get/get.dart';
+import 'package:twogass/apps/routes/route_names.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 class HomeController extends GetxController {
   final RxBool isLoading = false.obs;
   final RxnString error = RxnString();
+
+  addOrganization() {
+    YoLogger.error("Add Org");
+  }
+
+  joinOrganization() {
+    YoLogger.warning("Join Org");
+  }
+
+  detailOrganization(String orgId) {
+    Get.toNamed(RouteNames.ORGANIZATION, arguments: {"id": orgId});
+  }
 }

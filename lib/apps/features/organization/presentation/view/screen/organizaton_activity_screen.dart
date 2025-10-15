6039584971034fdfd07740cbc046f3df80d58958
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 08:39:00 +0700

    feat(organization): add dummy  for activity

--- lib/apps/features/organization/presentation/view/screen/organizaton_activity_screen.dart
+++ lib/apps/features/organization/presentation/view/screen/organizaton_activity_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:twogass/apps/core/helpers/activity_message.dart';
+import 'package:twogass/apps/data/model/activity_model.dart';
+import 'package:twogass/apps/widget/card_activity_widget.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../../controller/organization_controller.dart';
 
@@ -8,8 +12,44 @@ class OrganizatonActivityScreen extends GetView<OrganizationController> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Text('OrganizatonActivityScreen is working'),
+    return Scaffold(
+      body: ListView(
+        padding: YoPadding.all20,
+        children: [
+          CardActivityWidget(
+            model: ActivityModel.initial().copyWith(
+              title: "Created Organization",
+              description: ActivityMessageHelper.getActivityMessage(
+                context,
+                ActivityType.organizationCreated,
+                ActivityMeta(organizationName: "Dhuwitku", memberName: "Cahyo"),
+              ),
+            ),
+          ),
+          CardActivityWidget(
+            model: ActivityModel.initial().copyWith(
+              title: "Comment at",
+              type: ActivityType.commentAdded,
+              description: ActivityMessageHelper.getActivityMessage(
+                context,
+                ActivityType.commentAdded,
+                ActivityMeta(memberName: "Cahyo"),
+              ),
+            ),
+          ),
+          CardActivityWidget(
+            model: ActivityModel.initial().copyWith(
+              title: "Comment at",
+              type: ActivityType.taskCommented,
+              description: ActivityMessageHelper.getActivityMessage(
+                context,
+                ActivityType.taskCommented,
+                ActivityMeta(memberName: "Cahyo", taskName: "Kosong"),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

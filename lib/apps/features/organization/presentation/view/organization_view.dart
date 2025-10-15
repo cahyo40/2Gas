e18b6c3d2107fa585424701315c7f18b07ac13d8
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 20:32:06 +0700

    feat(org): - move listBottomNavBarItem from controller - add loading

--- lib/apps/features/organization/presentation/view/organization_view.dart
+++ lib/apps/features/organization/presentation/view/organization_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:stylish_bottom_bar/stylish_bottom_bar.dart';
 import 'package:yo_ui/yo_ui.dart';
 
@@ -10,25 +11,56 @@ class OrganizationView extends GetView<OrganizationController> {
 
   @override
   Widget build(BuildContext context) {
+    List<BottomBarItem> listBottomNavBarItem = [
+      BottomBarItem(
+        title: YoText.bodyMedium("Overview"),
+        icon: Icon(Iconsax.element_4_outline),
+        selectedColor: Color(controller.colorIcon.value),
+      ),
+      BottomBarItem(
+        title: YoText.bodyMedium("Project"),
+        icon: Icon(Iconsax.folder_open_outline),
+        selectedColor: Color(controller.colorIcon.value),
+      ),
+      BottomBarItem(
+        title: YoText.bodyMedium("Schedule"),
+        icon: Icon(Iconsax.calendar_2_outline),
+        selectedColor: Color(controller.colorIcon.value),
+      ),
+      BottomBarItem(
+        title: YoText.bodyMedium("Activity"),
+        icon: Icon(Iconsax.activity_outline),
+        selectedColor: Color(controller.colorIcon.value),
+      ),
+      BottomBarItem(
+        title: YoText.bodyMedium(tr.nav_settings),
+        icon: Icon(Iconsax.setting_2_outline),
+        selectedColor: Color(controller.colorIcon.value),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(),
       bottomNavigationBar: Obx(
-        () => StylishBottomBar(
-          option: BubbleBarOptions(barStyle: BubbleBarStyle.horizontal),
-          backgroundColor: context.backgroundColor,
-          currentIndex: controller.initialTab.value,
-          onTap: (i) => controller.changeTab(i),
-          items: controller.listBottomNavBarItem.map((b) {
-            return b;
-          }).toList(),
-        ),
+        () => controller.isLoading.isFalse
+            ? StylishBottomBar(
+                option: BubbleBarOptions(barStyle: BubbleBarStyle.horizontal),
+                backgroundColor: context.backgroundColor,
+                currentIndex: controller.initialTab.value,
+                onTap: (i) => controller.changeTab(i),
+                items: listBottomNavBarItem.map((b) {
+                  return b;
+                }).toList(),
+              )
+            : SizedBox.shrink(),
       ),
       body: SafeArea(
         child: Obx(
-          () => IndexedStack(
-            index: controller.initialTab.value,
-            children: controller.tabView,
-          ),
+          () => controller.isLoading.isFalse
+              ? IndexedStack(
+                  index: controller.initialTab.value,
+                  children: controller.tabView,
+                )
+              : Center(child: YoLoading()),
         ),
       ),
     );

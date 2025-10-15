130d19e6bb7c1d7c2d9c6fff692f6fa744386394
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 20:24:18 +0700

    feat(home-off-ds): implemenys HomeRepository

--- lib/apps/features/home/data/datasource/home_offline_datasource.dart
+++ lib/apps/features/home/data/datasource/home_offline_datasource.dart
@@ -1,3 +1,12 @@
-class HomeOfflineDatasource {
+import 'package:twogass/apps/features/home/data/models/organization_home_response.dart';
+import 'package:twogass/apps/features/home/domain/repositories/home_repository.dart';
+
+class HomeOfflineDatasource implements HomeRepository {
+  @override
+  Future<List<OrganizationHomeResponseModel>> homeOrganization() {
+    // TODO: implement homeOrganization
+    throw UnimplementedError();
+  }
+
   // TODO: local db / shared pref
 }

668a50cc3e535541d732e5cf4c122d33bab1bb75
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 08:33:40 +0700

    feat(firebase): add member & activity collection

--- lib/apps/core/services/firebase.dart
+++ lib/apps/core/services/firebase.dart
@@ -6,6 +6,7 @@ final collect = DatabaseConst();
 class FirebaseServices {
   FirebaseServices._();
 
+  static FirebaseFirestore get firestore => FirebaseFirestore.instance;
   static final FirebaseFirestore _fb = FirebaseFirestore.instance;
 
   static final users = _fb.collection(collect.user);
@@ -15,4 +16,6 @@ class FirebaseServices {
   static final schedule = _fb.collection(collect.schedule);
   static final notif = _fb.collection(collect.notification);
   static final voting = _fb.collection(collect.voting);
+  static final member = _fb.collection(collect.member);
+  static final activity = _fb.collection(collect.activity);
 }

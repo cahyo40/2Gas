eebe8fe1b8675569ee145581fb0908f5ed9caa47
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 08:45:49 +0700

    feat(l10n): add some sentences

--- lib/l10n/generated/app_localizations_en.dart
+++ lib/l10n/generated/app_localizations_en.dart
@@ -73,4 +73,179 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get good_night => 'Good night';
+
+  @override
+  String taskCreated(Object project, Object task, Object user) {
+    return '$user created task $task in project $project';
+  }
+
+  @override
+  String taskUpdated(Object task, Object user) {
+    return '$user updated task $task';
+  }
+
+  @override
+  String taskDeleted(Object task, Object user) {
+    return '$user deleted task $task';
+  }
+
+  @override
+  String taskCompleted(Object task, Object user) {
+    return '$user completed task $task';
+  }
+
+  @override
+  String taskReopened(Object task, Object user) {
+    return '$user reopened task $task';
+  }
+
+  @override
+  String taskAssigned(Object assignee, Object task, Object user) {
+    return '$user assigned task $task to $assignee';
+  }
+
+  @override
+  String taskUnassigned(Object task, Object user) {
+    return '$user unassigned task $task';
+  }
+
+  @override
+  String taskMoved(Object column, Object task, Object user) {
+    return '$user moved task $task to column $column';
+  }
+
+  @override
+  String taskCommented(Object task, Object user) {
+    return '$user commented on task $task';
+  }
+
+  @override
+  String taskAttachmentAdded(Object task, Object user) {
+    return '$user added attachment to task $task';
+  }
+
+  @override
+  String taskAttachmentRemoved(Object task, Object user) {
+    return '$user removed attachment from task $task';
+  }
+
+  @override
+  String taskLabelAdded(Object label, Object task, Object user) {
+    return '$user added label $label to task $task';
+  }
+
+  @override
+  String taskLabelRemoved(Object label, Object task, Object user) {
+    return '$user removed label $label from task $task';
+  }
+
+  @override
+  String projectCreated(Object project, Object user) {
+    return '$user created project $project';
+  }
+
+  @override
+  String projectUpdated(Object project, Object user) {
+    return '$user updated project $project';
+  }
+
+  @override
+  String projectDeleted(Object project, Object user) {
+    return '$user deleted project $project';
+  }
+
+  @override
+  String projectArchived(Object project, Object user) {
+    return '$user archived project $project';
+  }
+
+  @override
+  String projectRestored(Object project, Object user) {
+    return '$user restored project $project';
+  }
+
+  @override
+  String memberInvited(Object org, Object target, Object user) {
+    return '$user invited $target to $org';
+  }
+
+  @override
+  String memberJoined(Object org, Object user) {
+    return '$user joined $org';
+  }
+
+  @override
+  String memberRemoved(Object org, Object target, Object user) {
+    return '$user removed $target from $org';
+  }
+
+  @override
+  String memberRoleUpdated(Object role, Object target, Object user) {
+    return '$user updated $target\'s role to $role';
+  }
+
+  @override
+  String organizationCreated(Object org, Object user) {
+    return '$user created new organization $org';
+  }
+
+  @override
+  String organizationUpdated(Object org, Object user) {
+    return '$user updated organization $org';
+  }
+
+  @override
+  String organizationDeleted(Object org, Object user) {
+    return '$user deleted organization $org';
+  }
+
+  @override
+  String organizationJoined(Object org, Object user) {
+    return '$user joined organization $org';
+  }
+
+  @override
+  String organizationLeft(Object org, Object user) {
+    return '$user left organization $org';
+  }
+
+  @override
+  String commentAdded(Object user) {
+    return '$user added a comment';
+  }
+
+  @override
+  String commentEdited(Object user) {
+    return '$user edited a comment';
+  }
+
+  @override
+  String commentDeleted(Object user) {
+    return '$user deleted a comment';
+  }
+
+  @override
+  String attachmentAdded(Object file, Object user) {
+    return '$user added attachment $file';
+  }
+
+  @override
+  String attachmentRemoved(Object file, Object user) {
+    return '$user removed attachment $file';
+  }
+
+  @override
+  String labelCreated(Object label, Object user) {
+    return '$user created new label $label';
+  }
+
+  @override
+  String labelUpdated(Object label, Object user) {
+    return '$user updated label $label';
+  }
+
+  @override
+  String labelDeleted(Object label, Object user) {
+    return '$user deleted label $label';
+  }
 }

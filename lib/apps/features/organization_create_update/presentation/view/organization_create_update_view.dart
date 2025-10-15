59d696e150c161b930ea8eb87722386706388fce
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 08:43:12 +0700

    feat(org-create-update): add multi-step form view with field screens

---
+++ lib/apps/features/organization_create_update/presentation/view/organization_create_update_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:twogass/apps/features/organization_create_update/presentation/view/screen/organization_field_address_screen.dart';
+import 'package:twogass/apps/features/organization_create_update/presentation/view/screen/organization_field_colors_screen.dart';
+import 'package:twogass/apps/features/organization_create_update/presentation/view/screen/organization_field_desc_screen.dart';
+import 'package:twogass/apps/features/organization_create_update/presentation/view/screen/organization_field_email_screen.dart';
+import 'package:twogass/apps/features/organization_create_update/presentation/view/screen/organization_field_picture_screen.dart';
+import 'package:twogass/apps/features/organization_create_update/presentation/view/screen/organization_field_title_screen.dart';
+import 'package:yo_ui/yo_ui.dart';
+
+import '../controller/organization_create_update_controller.dart';
+
+class OrganizationCreateUpdateView
+    extends GetView<OrganizationCreateUpdateController> {
+  const OrganizationCreateUpdateView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: YoText.titleMedium("Create Organization")),
+      body: SafeArea(
+        child: Form(
+          key: controller.formKey,
+          child: ListView(
+            padding: YoPadding.all20,
+            children: [
+              OrganizationFieldTitleScreen(),
+              OrganizationFieldEmailScreen(),
+              OrganizationFieldAddressScreen(),
+              Row(
+                children: [
+                  Expanded(child: OrganizationFieldPictureScreen()),
+                  Expanded(child: OrganizationFieldColorsScreen()),
+                ],
+              ),
+              OrganizationFieldDescScreen(),
+              Obx(
+                () => YoButton.primary(
+                  text: "Submit",
+                  isLoading: controller.isLoading.value,
+                  onPressed: () {
+                    controller.onSubmit();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

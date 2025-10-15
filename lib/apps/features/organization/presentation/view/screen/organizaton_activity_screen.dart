f7e39f0c6645a34992c7bbd4d93148697293eb6b
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 20:33:08 +0700

    feat(org-activity): remove dummy and implements from database

--- lib/apps/features/organization/presentation/view/screen/organizaton_activity_screen.dart
+++ lib/apps/features/organization/presentation/view/screen/organizaton_activity_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:twogass/apps/core/helpers/activity_message.dart';
-import 'package:twogass/apps/data/model/activity_model.dart';
 import 'package:twogass/apps/widget/card_activity_widget.dart';
 import 'package:yo_ui/yo_ui.dart';
 
@@ -13,42 +11,17 @@ class OrganizatonActivityScreen extends GetView<OrganizationController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: YoPadding.all20,
-        children: [
-          CardActivityWidget(
-            model: ActivityModel.initial().copyWith(
-              title: "Created Organization",
-              description: ActivityMessageHelper.getActivityMessage(
-                context,
-                ActivityType.organizationCreated,
-                ActivityMeta(organizationName: "Dhuwitku", memberName: "Cahyo"),
-              ),
-            ),
-          ),
-          CardActivityWidget(
-            model: ActivityModel.initial().copyWith(
-              title: "Comment at",
-              type: ActivityType.commentAdded,
-              description: ActivityMessageHelper.getActivityMessage(
-                context,
-                ActivityType.commentAdded,
-                ActivityMeta(memberName: "Cahyo"),
-              ),
-            ),
-          ),
-          CardActivityWidget(
-            model: ActivityModel.initial().copyWith(
-              title: "Comment at",
-              type: ActivityType.taskCommented,
-              description: ActivityMessageHelper.getActivityMessage(
-                context,
-                ActivityType.taskCommented,
-                ActivityMeta(memberName: "Cahyo", taskName: "Kosong"),
-              ),
-            ),
-          ),
-        ],
+      body: Obx(
+        () => ListView.builder(
+          padding: YoPadding.all20,
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: controller.activity.length,
+          itemBuilder: (_, i) {
+            final model = controller.activity[i];
+            return CardActivityWidget(model: model);
+          },
+        ),
       ),
     );
   }

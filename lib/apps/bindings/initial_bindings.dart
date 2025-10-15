cfd96f874f5358fb25dcbf117efc577e5839fd76
cahyo40 <mdcnugroho@gmail.com>
2025-10-15 20:20:21 +0700

    feat(initial-binding): add inject home & organization

--- lib/apps/bindings/initial_bindings.dart
+++ lib/apps/bindings/initial_bindings.dart
@@ -1,9 +1,17 @@
 import 'package:get/get.dart';
 import 'package:twogass/apps/core/services/sembast.dart';
+import 'package:twogass/apps/features/home/data/datasource/home_network_datasource.dart';
+import 'package:twogass/apps/features/home/data/datasource/home_offline_datasource.dart';
+import 'package:twogass/apps/features/home/data/repositories/home_repository_impl.dart';
+import 'package:twogass/apps/features/home/domain/repositories/home_repository.dart';
 import 'package:twogass/apps/features/login/data/datasource/login_network_datasource.dart';
 import 'package:twogass/apps/features/login/data/datasource/login_offline_datasource.dart';
 import 'package:twogass/apps/features/login/data/repositories/login_repository_impl.dart';
 import 'package:twogass/apps/features/login/domain/repositories/login_repository.dart';
+import 'package:twogass/apps/features/organization/data/datasource/organization_network_datasource.dart';
+import 'package:twogass/apps/features/organization/data/datasource/organization_offline_datasource.dart';
+import 'package:twogass/apps/features/organization/data/repositories/organization_repository_impl.dart';
+import 'package:twogass/apps/features/organization/domain/repositories/organization_repository.dart';
 import 'package:twogass/apps/features/organization_create_update/data/datasource/organization_create_update_network_datasource.dart';
 import 'package:twogass/apps/features/organization_create_update/data/datasource/organization_create_update_offline_datasource.dart';
 import 'package:twogass/apps/features/organization_create_update/data/repositories/organization_create_update_repository_impl.dart';
@@ -42,6 +50,31 @@ class InitialBindings implements Bindings {
       fenix: true,
     );
 
+    // HOME
+    Get.lazyPut<HomeRepository>(
+      () => HomeRepositoryImpl(
+        Get.find<HomeNetworkDatasource>(),
+        Get.find<HomeOfflineDatasource>(),
+      ),
+      fenix: true,
+    );
+
+    Get.lazyPut(() => HomeNetworkDatasource(), fenix: true);
+    Get.lazyPut(() => HomeOfflineDatasource(), fenix: true);
+
+    // ORGANIZATION
+    Get.lazyPut<OrganizationRepository>(
+      () => OrganizationRepositoryImpl(
+        Get.find<OrganizationNetworkDatasource>(),
+        Get.find<OrganizationOfflineDatasource>(),
+      ),
+      fenix: true,
+    );
+
+    Get.lazyPut(() => OrganizationNetworkDatasource(), fenix: true);
+    Get.lazyPut(() => OrganizationOfflineDatasource(), fenix: true);
+
+    // ORGANIZATION Create Update
     Get.lazyPut<OrganizationCreateUpdateRepository>(
       () => OrganizationCreateUpdateRepositoryImpl(
         Get.find<OrganizationCreateUpdateNetworkDatasource>(),

16f4b8a679e95c8bb8dd83f3498f3a45c0164b78
cahyo40 <mdcnugroho@gmail.com>
2025-10-17 08:23:42 +0700

    feat(constants): add projectAssign, taskAssign, and category collections to DatabaseConst

--- lib/apps/core/constants/database.dart
+++ lib/apps/core/constants/database.dart
@@ -2,11 +2,14 @@ class DatabaseConst {
   String user = "user";
   String org = "organization";
   String project = "project";
+  String projectAssign = "project-assign";
   String task = "task";
+  String taskAssign = "task-assign";
   String schedule = "schedule";
   String notification = "notification";
   String voting = "voting";
   String member = "member";
   String activity = "activity";
+  String category = "category";
   // static String user = "user";
 }

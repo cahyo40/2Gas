e2c09a058482f5bcd32f89a49117f014504456bd
cahyo40 <mdcnugroho@gmail.com>
2025-10-17 08:45:22 +0700

    feat(project-create-repo): - create function - implements function from repo - implements function from datasource

--- lib/apps/features/project_create/data/repositories/project_create_repository_impl.dart
+++ lib/apps/features/project_create/data/repositories/project_create_repository_impl.dart
@@ -1,3 +1,6 @@
+import 'package:twogass/apps/data/model/project_category_model.dart';
+import 'package:twogass/apps/data/model/project_model.dart';
+
 import '../../domain/repositories/project_create_repository.dart';
 import '../datasource/project_create_network_datasource.dart';
 import '../datasource/project_create_offline_datasource.dart';
@@ -7,4 +10,19 @@ class ProjectCreateRepositoryImpl implements ProjectCreateRepository {
   final ProjectCreateOfflineDatasource _offline;
 
   ProjectCreateRepositoryImpl(this._network, this._offline);
+
+  @override
+  Future<void> createCategory(ProjectCategoryModel categories) async {
+    await _network.createCategory(categories);
+  }
+
+  @override
+  Future<Map<String, dynamic>> getMemberCategory(String orgId) async {
+    return _network.getMemberCategory(orgId);
+  }
+
+  @override
+  Future<bool> submitProject(ProjectModel project) async {
+    return _network.submitProject(project);
+  }
 }

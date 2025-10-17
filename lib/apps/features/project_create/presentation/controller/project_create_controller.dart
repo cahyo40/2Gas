c35ff1668683abab727ec6d64b56aefea08b6ea2
cahyo40 <mdcnugroho@gmail.com>
2025-10-17 08:47:06 +0700

    feat(project-create-cont): update controller

--- lib/apps/features/project_create/presentation/controller/project_create_controller.dart
+++ lib/apps/features/project_create/presentation/controller/project_create_controller.dart
@@ -1,6 +1,143 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:twogass/apps/controller/auth_controller.dart';
+import 'package:twogass/apps/core/constants/database.dart';
+import 'package:twogass/apps/data/model/member_model.dart';
+import 'package:twogass/apps/data/model/project_category_model.dart';
+import 'package:twogass/apps/data/model/project_model.dart';
+import 'package:twogass/apps/data/model/task_model.dart';
+import 'package:twogass/apps/features/project_create/domain/usecase/member_category_project_usecase.dart';
+import 'package:twogass/apps/features/project_create/domain/usecase/project_create_usecase.dart';
+import 'package:yo_ui/yo_ui_base.dart';
 
 class ProjectCreateController extends GetxController {
   final RxBool isLoading = false.obs;
   final RxnString error = RxnString();
+  final user = Get.find<AuthController>();
+  final id = YoIdGenerator.alphanumericId();
+  final formKey = GlobalKey<FormState>();
+  final RxString orgId = "".obs;
+
+  final name = TextEditingController();
+  // final catCtrl = "".obs;
+  final catCtrl = TextEditingController();
+  final category = <ProjectCategoryModel>[].obs;
+  final desc = TextEditingController();
+  final deadlineCtrl = TextEditingController();
+  final RxList<ProjectAssignModel> assigns = <ProjectAssignModel>[].obs;
+  final Rx<Priority> priority = Priority.low.obs;
+  final Rx<DateTime> deadline = DateTime.now().obs;
+  final Rx<ProjectStatus> status = ProjectStatus.active.obs;
+
+  final initialMember = <MemberModel>[].obs;
+  final initialCategory = <ProjectCategoryModel>[].obs;
+
+  ProjectCreateUsecase create = ProjectCreateUsecase(Get.find());
+
+  GetMemberCategoryProjectUseCase memberCategory =
+      GetMemberCategoryProjectUseCase(Get.find());
+
+  initialData() async {
+    isLoading.value = true;
+    try {
+      final data = await memberCategory(orgId.value);
+      initialMember.addAll(data[DatabaseConst().member]);
+      initialCategory.addAll(data[DatabaseConst().category]);
+      initialMember.refresh();
+      initialCategory.refresh();
+    } catch (_) {
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  onAddCategory(String value) {
+    final data = ProjectCategoryModel(
+      id: YoIdGenerator.alphanumericId(),
+      orgId: orgId.value,
+      name: value.trim(),
+    );
+    category.add(data);
+    category.refresh();
+  }
+
+  onSelectCategory(ProjectCategoryModel model) {
+    category.add(model);
+    category.refresh();
+  }
+
+  onAssignMember(MemberModel member) {
+    if (assigns.any((f) => f.uid.contains(member.id))) {
+      assigns.removeWhere((e) => e.uid.contains(member.id));
+    } else {
+      final data = ProjectAssignModel(
+        id: YoIdGenerator.alphanumericId(),
+        projectId: id,
+        uid: member.id,
+        imageUrl: member.imageUrl,
+      );
+      assigns.add(data);
+    }
+    assigns.refresh();
+  }
+
+  bool isAssignMember(MemberModel member) {
+    return assigns.any((f) => f.uid.contains(member.id));
+  }
+
+  Priority onSelectPriority(String value) {
+    switch (value) {
+      case "low":
+        return Priority.low;
+      case "medium":
+        return Priority.medium;
+      default:
+        return Priority.high;
+    }
+  }
+
+  onSumbit() async {
+    if (formKey.currentState!.validate()) {
+      isLoading.value = true;
+      try {
+        final now = DateTime.now();
+        final project = ProjectModel(
+          id: id,
+          name: name.text.trim(), // field
+          orgId: orgId.value,
+          priority: priority.value, // field
+          status: status.value, // default active
+          deadline: deadline.value, // field
+          createdAt: now, //default
+          createdBy: user.uid, //default
+          assign: assigns, // field
+          description: desc.text.trim(), // field
+          categories: category,
+        );
+        await create(project);
+
+        Get.back(result: true);
+      } catch (e, s) {
+        YoSnackBar.show(
+          context: Get.context!,
+          message: "Error : $e -> $s",
+          type: YoSnackBarType.error,
+        );
+      } finally {
+        isLoading.value = false;
+      }
+    }
+  }
+
+  @override
+  void onInit() {
+    final args = Get.arguments;
+    if (args is Map<String, dynamic> && args['orgId'] != null) {
+      orgId.value = args['orgId'] as String;
+      initialData();
+    } else {
+      // opsional: kembali atau tampilkan error
+    }
+    super.onInit();
+  }
 }

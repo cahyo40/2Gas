1ee648b30a9c132f9ef04bd4d2905d5ab8426da0
cahyo40 <mdcnugroho@gmail.com>
2025-10-18 08:45:22 +0700

    feat(project): testing view

--- lib/apps/features/project/presentation/view/project_view.dart
+++ lib/apps/features/project/presentation/view/project_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../controller/project_controller.dart';
 
@@ -8,19 +9,23 @@ class ProjectView extends GetView<ProjectController> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('ProjectView'.tr),
-          centerTitle: true,
-        ),
-        body: const SafeArea(
-          child: Text(
-            'ProjectView is working',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
+    return Obx(
+      () => controller.isLoading.isFalse
+          ? Scaffold(
+              appBar: AppBar(
+                title: YoText.titleMedium(
+                  controller.project.value.name.capitalize!,
+                ),
+                centerTitle: true,
+              ),
+              body: SafeArea(
+                child: Text(
+                  'ProjectView is working ${controller.id.value}',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            )
+          : Scaffold(body: Center(child: YoLoading())),
     );
   }
 }

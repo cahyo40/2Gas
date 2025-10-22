b897ce26f08f2bff65c410cf68803e9770216b0a
cahyo40 <mdcnugroho@gmail.com>
2025-10-22 11:52:35 +0700

    feat(project): add refresh data, endrawer for member

--- lib/apps/features/project/presentation/view/project_view.dart
+++ lib/apps/features/project/presentation/view/project_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:twogass/apps/features/project/presentation/view/screen/project_assign_screen.dart';
+import 'package:twogass/apps/features/project/presentation/view/screen/project_comments_screen.dart';
+import 'package:twogass/apps/features/project/presentation/view/screen/project_detail_screen.dart';
+import 'package:twogass/apps/features/project/presentation/view/screen/project_task_screen.dart';
 import 'package:yo_ui/yo_ui.dart';
 
 import '../controller/project_controller.dart';
@@ -9,23 +14,58 @@ class ProjectView extends GetView<ProjectController> {
 
   @override
   Widget build(BuildContext context) {
+    final key = GlobalKey<ScaffoldState>();
     return Obx(
       () => controller.isLoading.isFalse
-          ? Scaffold(
-              appBar: AppBar(
-                title: YoText.titleMedium(
-                  controller.project.value.name.capitalize!,
+          ? RefreshIndicator(
+              onRefresh: () async {
+                controller.initData();
+              },
+              child: Scaffold(
+                key: key,
+                endDrawer: Drawer(
+                  backgroundColor: context.backgroundColor,
+                  width: Get.width * 0.8,
+                  child: ProjectAssignScreen(),
                 ),
-                centerTitle: true,
-              ),
-              body: SafeArea(
-                child: Text(
-                  'ProjectView is working ${controller.id.value}',
-                  style: TextStyle(fontSize: 20),
+                appBar: AppBar(
+                  title: YoText.titleLarge(
+                    controller.project.value.name.capitalize!,
+                  ),
+                  centerTitle: true,
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        key.currentState!.openEndDrawer();
+                      },
+                      icon: Icon(Iconsax.profile_2user_outline),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Iconsax.setting_2_outline),
+                    ),
+                  ],
+                ),
+                body: SafeArea(
+                  child: ListView(
+                    padding: YoPadding.all20,
+                    physics: AlwaysScrollableScrollPhysics(),
+                    children: [
+                      ProjectDetailScreen(),
+                      SizedBox(height: YoSpacing.md),
+                      ProjectTaskScreen(),
+                      SizedBox(height: YoSpacing.md),
+                      ProjectCommentsScreen(),
+                    ],
+                  ),
                 ),
               ),
             )
-          : Scaffold(body: Center(child: YoLoading())),
+          : Scaffold(
+              body: Center(
+                child: YoLoading.spinner(color: context.primaryColor),
+              ),
+            ),
     );
   }
 }

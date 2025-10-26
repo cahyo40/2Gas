e9cbe62ed347e029a1b6b19fa5c9ba6a42169529
cahyo40 <mdcnugroho@gmail.com>
2025-10-26 08:14:15 +0700

    feat(home-repo): add userTask

--- lib/apps/features/home/domain/repositories/home_repository.dart
+++ lib/apps/features/home/domain/repositories/home_repository.dart
@@ -1,5 +1,7 @@
+import 'package:twogass/apps/data/model/task_model.dart';
 import 'package:twogass/apps/features/home/data/models/organization_home_response.dart';
 
 abstract class HomeRepository {
   Future<List<OrganizationHomeResponseModel>> homeOrganization();
+  Future<List<TaskModel>> userTask();
 }

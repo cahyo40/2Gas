1ede3426498dc54b46e7b423a3f6fc0ab4eb671b
cahyo40 <mdcnugroho@gmail.com>
2025-10-26 08:31:08 +0700

    feat(task-create-ds-net): implements methods from task create repository and create function create task

--- lib/apps/features/task_create/data/datasource/task_create_network_datasource.dart
+++ lib/apps/features/task_create/data/datasource/task_create_network_datasource.dart
@@ -1,3 +1,50 @@
-class TaskCreateNetworkDatasource {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'package:twogass/apps/controller/auth_controller.dart';
+import 'package:twogass/apps/core/services/firebase.dart';
+import 'package:twogass/apps/data/model/activity_model.dart';
+import 'package:twogass/apps/data/model/task_model.dart';
+import 'package:twogass/apps/features/task_create/domain/repositories/task_create_repository.dart';
+import 'package:yo_ui/yo_ui.dart';
+
+class TaskCreateNetworkDatasource implements TaskCreateRepository {
+  final user = Get.find<AuthController>();
+  @override
+  Future<bool> createTask(TaskModel task) async {
+    try {
+      final idActivity = YoIdGenerator.alphanumericId();
+      final activity = ActivityModel(
+        createdAt: task.createdAt,
+        orgId: task.orgId,
+        projectId: task.projectId,
+        id: idActivity,
+        title: 'Task Created',
+        type: ActivityType.taskCreated,
+        description: "",
+        meta: ActivityMeta(
+          projectName: task.name,
+          taskName: task.name,
+          organizationName: user.name,
+        ),
+      );
+
+      await FirebaseServices.task.doc(task.id).set(task.toJson());
+      await FirebaseServices.activity.doc(idActivity).set(activity.toJson());
+      await Future.wait(
+        task.assigns.map(
+          (a) => FirebaseServices.taskAssign.doc(a.id).set(a.toJson()),
+        ),
+      );
+
+      return true;
+    } on FirebaseException catch (e, s) {
+      YoLogger.error('Firestore error $e -> $s');
+      return false;
+    } catch (e, s) {
+      YoLogger.error('Unexpected error  $e-> $s');
+      return false;
+    }
+  }
+
   // TODO: remote api calls
 }

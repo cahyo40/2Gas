6ebee53be7211bebd85656a0cec5269416bda663
cahyo40 <mdcnugroho@gmail.com>
2025-10-26 08:21:49 +0700

    feat(org-con): add refresh project and add parameters useLoading at initOrg

--- lib/apps/features/organization/presentation/controller/organization_controller.dart
+++ lib/apps/features/organization/presentation/controller/organization_controller.dart
@@ -57,8 +57,8 @@ class OrganizationController extends GetxController {
     initialTab.value = i;
   }
 
-  initOrg(String orgId) async {
-    isLoading.value = true;
+  initOrg(String orgId, {bool useLoading = true}) async {
+    isLoading.value = useLoading;
     try {
       org.value = await getOrganizationUsecase(orgId);
       activity.value = await getActivity(orgId);
@@ -69,6 +69,13 @@ class OrganizationController extends GetxController {
     } finally {
       isLoading.value = false;
     }
+  }
+
+  refreshProject() async {
+    try {
+      project.value = await getProject(orgId.value);
+      projectShow.value = project;
+    } catch (_) {}
   }
 
   final filtersProject = ["all", "active", "completed", "overdue"];

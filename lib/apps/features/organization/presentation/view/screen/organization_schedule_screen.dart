0fff71b7b79611491c99d192ce0e60fe44b0159d
cahyo40 <mdcnugroho@gmail.com>
2025-10-28 07:07:20 +0700

    chore(org-schedule): generate organization schedule screen

---
+++ lib/apps/features/organization/presentation/view/screen/organization_schedule_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../controller/organization_controller.dart';
+
+class OrganizationScheduleScreen extends GetView<OrganizationController> {
+  const OrganizationScheduleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child: Text('OrganizationScheduleScreen is working'));
+  }
+}

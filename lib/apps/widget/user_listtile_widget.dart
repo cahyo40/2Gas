154114c49b251ccd7ea8dd74908bc9b2ae49edeb
cahyo40 <mdcnugroho@gmail.com>
2025-10-28 07:13:50 +0700

    style(user-listtile): change style and add some parameters

--- lib/apps/widget/user_listtile_widget.dart
+++ lib/apps/widget/user_listtile_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
 import 'package:twogass/apps/core/services/firebase.dart';
 import 'package:twogass/apps/data/model/user_model.dart';
 import 'package:yo_ui/yo_ui_base.dart';
@@ -8,11 +7,16 @@ class UserListtileWidget extends StatelessWidget {
   final String uid;
   final bool? isSelected;
   final void Function()? onTap;
+  final UserListTileSize size;
+  final bool? showAvatar;
+
   const UserListtileWidget({
     super.key,
     required this.uid,
     this.isSelected = false,
     this.onTap,
+    this.size = UserListTileSize.medium,
+    this.showAvatar = true,
   });
 
   @override
@@ -29,25 +33,159 @@ class UserListtileWidget extends StatelessWidget {
       future: getUser(uid),
       builder: (_, snapshot) {
         final user = snapshot.data;
+
+        final avatarSize = _getAvatarSize();
+        final textStyle = _getTextStyle(context);
+        final padding = _getPadding();
+
         return Padding(
           padding: YoPadding.onlyBottom8,
-          child: ListTile(
-            onTap: onTap,
-            title: YoText.bodyMedium(
-              user?.name ?? "",
-              maxLines: 1,
-              softWrap: true,
-              overflow: TextOverflow.ellipsis,
+          child: Material(
+            color: isSelected == true
+                ? context.primaryColor.withOpacity(0.1)
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(_getBorderRadius()),
+            child: InkWell(
+              onTap: onTap,
+              borderRadius: BorderRadius.circular(_getBorderRadius()),
+              child: Container(
+                padding: padding,
+                decoration: BoxDecoration(
+                  border: isSelected == true
+                      ? Border.all(color: context.primaryColor.withOpacity(0.3))
+                      : null,
+                  borderRadius: BorderRadius.circular(_getBorderRadius()),
+                ),
+                child: Row(
+                  children: [
+                    // Avatar
+                    showAvatar == true
+                        ? user?.photoUrl == null || user?.photoUrl == ""
+                              ? Container(
+                                  width: avatarSize,
+                                  height: avatarSize,
+                                  decoration: BoxDecoration(
+                                    color: context.gray200,
+                                    shape: BoxShape.circle,
+                                  ),
+                                  child: Icon(
+                                    Icons.person,
+                                    size: avatarSize * 0.6,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                )
+                              : Container(
+                                  width: avatarSize,
+                                  height: avatarSize,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                      image: NetworkImage(user!.photoUrl!),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                )
+                        : SizedBox(),
+
+                    SizedBox(width: _getSpacing()),
+
+                    // User info
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            user?.name ?? "please wait...",
+                            style: textStyle,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Selection indicator
+                    if (isSelected == true)
+                      Icon(
+                        Icons.check_circle_rounded,
+                        color: context.successColor,
+                        size: _getIconSize(),
+                      ),
+                  ],
+                ),
+              ),
             ),
-            leading: user?.photoUrl == null || user?.photoUrl == ""
-                ? CircleAvatar(backgroundColor: context.gray200)
-                : CircleAvatar(backgroundImage: NetworkImage(user!.photoUrl!)),
-            trailing: isSelected == true
-                ? Icon(Iconsax.tick_circle_outline, color: context.successColor)
-                : SizedBox(),
           ),
         );
       },
     );
   }
+
+  double _getAvatarSize() {
+    switch (size) {
+      case UserListTileSize.small:
+        return 32;
+      case UserListTileSize.medium:
+        return 40;
+      case UserListTileSize.large:
+        return 48;
+    }
+  }
+
+  double _getSpacing() {
+    switch (size) {
+      case UserListTileSize.small:
+        return 8;
+      case UserListTileSize.medium:
+        return 12;
+      case UserListTileSize.large:
+        return 16;
+    }
+  }
+
+  double _getIconSize() {
+    switch (size) {
+      case UserListTileSize.small:
+        return 16;
+      case UserListTileSize.medium:
+        return 20;
+      case UserListTileSize.large:
+        return 24;
+    }
+  }
+
+  double _getBorderRadius() {
+    switch (size) {
+      case UserListTileSize.small:
+        return 8;
+      case UserListTileSize.medium:
+        return 12;
+      case UserListTileSize.large:
+        return 16;
+    }
+  }
+
+  EdgeInsets _getPadding() {
+    switch (size) {
+      case UserListTileSize.small:
+        return EdgeInsets.symmetric(horizontal: 8, vertical: 6);
+      case UserListTileSize.medium:
+        return EdgeInsets.symmetric(horizontal: 12, vertical: 8);
+      case UserListTileSize.large:
+        return EdgeInsets.symmetric(horizontal: 16, vertical: 12);
+    }
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    switch (size) {
+      case UserListTileSize.small:
+        return context.yoBodySmall.copyWith(fontWeight: FontWeight.w500);
+      case UserListTileSize.medium:
+        return context.yoBodyMedium.copyWith(fontWeight: FontWeight.w500);
+      case UserListTileSize.large:
+        return context.yoBodyLarge.copyWith(fontWeight: FontWeight.w600);
+    }
+  }
 }
+
+enum UserListTileSize { small, medium, large }

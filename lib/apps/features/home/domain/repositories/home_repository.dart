9749f6609778e017e2669a7763eb025256a2c7ee
cahyo40 <mdcnugroho@gmail.com>
2025-10-29 20:10:57 +0700

    feat(home-repo): add some function

--- lib/apps/features/home/domain/repositories/home_repository.dart
+++ lib/apps/features/home/domain/repositories/home_repository.dart
@@ -1,7 +1,11 @@
+import 'package:twogass/apps/data/model/organitation_model.dart';
 import 'package:twogass/apps/data/model/task_model.dart';
 import 'package:twogass/apps/features/home/data/models/organization_home_response.dart';
 
 abstract class HomeRepository {
   Future<List<OrganizationHomeResponseModel>> homeOrganization();
   Future<List<TaskModel>> userTask();
+  Future<List<OrganizationModel>> getOrganization(String orgCode);
+  Future<void> joinOrganization(String orgId);
+  Future<bool> isJoinOrganization(String orgId);
 }

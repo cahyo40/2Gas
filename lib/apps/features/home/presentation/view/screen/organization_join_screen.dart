503f3897c244b3b9aaa24d394c2f1c5bf7b1a45b
cahyo40 <mdcnugroho@gmail.com>
2025-10-29 20:13:17 +0700

    feat(org-join-screen): create join organiziation screen

---
+++ lib/apps/features/home/presentation/view/screen/organization_join_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:twogass/apps/features/home/presentation/controller/home_controller.dart';
+import 'package:yo_ui/yo_ui.dart';
+
+class OrganizationJoinScreen extends GetView<HomeController> {
+  const OrganizationJoinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Form(
+              key: controller.key,
+              child: Row(
+                spacing: context.yoSpacingMd,
+                children: [
+                  Expanded(
+                    child: YoTextFormField(
+                      controller: controller.code,
+                      inputType: YoInputType.search,
+                      inputStyle: YoInputStyle.modern,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      controller.onGetOrgByCode(controller.code.text);
+                    },
+                    icon: Icon(Iconsax.search_normal_1_outline),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Obx(
+                () => controller.isLoading.isTrue
+                    ? Center(child: YoLoading())
+                    : controller.orgs.isEmpty
+                    ? YoEmptyState.noData()
+                    : ListView.builder(
+                        itemCount: controller.orgs.length,
+                        itemBuilder: (_, i) {
+                          final org = controller.orgs[i];
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundColor: Color(
+                                org.color ?? context.primaryColor.toARGB32(),
+                              ),
+                            ),
+                            subtitle: YoText.bodySmall(org.description ?? ""),
+                            title: YoText.titleMedium(org.name),
+                            trailing: FutureBuilder<bool>(
+                              future: controller.isJoinOrg(org.id),
+                              builder: (context, asyncSnapshot) {
+                                final isJoined = asyncSnapshot.data ?? false;
+
+                                if (isJoined) {
+                                  return YoText.bodyMedium(
+                                    "Joined",
+                                    color: context.primaryColor,
+                                    fontWeight: FontWeight.w600,
+                                  );
+                                } else {
+                                  return YoButton.primary(
+                                    text: "Bergabung",
+                                    size: YoButtonSize.small,
+                                    textColor: context.onPrimaryBW,
+                                    onPressed: () {
+                                      controller.onJoinOrg(org.id);
+                                    },
+                                  );
+                                }
+                              },
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

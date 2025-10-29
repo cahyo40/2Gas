14a71ee4cc52694704733e123aca5c3e32d92640
cahyo40 <mdcnugroho@gmail.com>
2025-10-29 20:10:03 +0700

    feta(home-off-ds): implments homeRepository

--- lib/apps/features/home/data/datasource/home_offline_datasource.dart
+++ lib/apps/features/home/data/datasource/home_offline_datasource.dart
@@ -1,3 +1,4 @@
+import 'package:twogass/apps/data/model/organitation_model.dart';
 import 'package:twogass/apps/data/model/task_model.dart';
 import 'package:twogass/apps/features/home/data/models/organization_home_response.dart';
 import 'package:twogass/apps/features/home/domain/repositories/home_repository.dart';
@@ -18,6 +19,24 @@ class HomeOfflineDatasource implements HomeRepository {
   @override
   Future<List<TaskModel>> userTask() {
     // TODO: implement userTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<OrganizationModel>> getOrganization(String orgCode) {
+    // TODO: implement getOrganization
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> joinOrganization(String orgId) {
+    // TODO: implement joinOrganization
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isJoinOrganization(String orgId) {
+    // TODO: implement isJoinOrganization
     throw UnimplementedError();
   }
 

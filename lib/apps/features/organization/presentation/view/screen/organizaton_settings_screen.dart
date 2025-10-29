f62667a2d639e82896fd8155314dc454ddd7bf4c
cahyo40 <mdcnugroho@gmail.com>
2025-10-29 20:17:46 +0700

    feat(org-settings): add button for navigate to member

--- lib/apps/features/organization/presentation/view/screen/organizaton_settings_screen.dart
+++ lib/apps/features/organization/presentation/view/screen/organizaton_settings_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:twogass/apps/features/organization/presentation/view/screen/org_settings_member_screen.dart';
+import 'package:yo_ui/yo_ui.dart';
 
 import '../../controller/organization_controller.dart';
 
@@ -8,8 +11,28 @@ class OrganizatonSettingsScreen extends GetView<OrganizationController> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Text('OrganizatonSettingsScreen is working'),
+    return SafeArea(
+      child: Padding(
+        padding: YoPadding.all20,
+        child: Column(
+          children: [
+            ListTile(
+              title: YoText("Member"),
+              leading: Icon(Iconsax.profile_2user_outline),
+              onTap: () {
+                controller.membersFilter();
+                Get.to(() => OrgSettingsMemberScreen());
+              },
+            ),
+            Spacer(),
+            ListTile(
+              title: YoText("Back to home"),
+              leading: Icon(Iconsax.home_1_outline),
+              onTap: () => Get.back(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

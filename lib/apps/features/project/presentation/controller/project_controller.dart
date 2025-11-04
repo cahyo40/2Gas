f971f80a3d73352209d6af7498706a167c2e2f3c
cahyo40 <mdcnugroho@gmail.com>
2025-11-04 22:00:04 +0700

    feat(project-controller): add members (variable), getMember, addAssign (call usecase), goToAssignProject(navigation), addAssigner(function)

--- lib/apps/features/project/presentation/controller/project_controller.dart
+++ lib/apps/features/project/presentation/controller/project_controller.dart
@@ -1,13 +1,17 @@
 import 'package:get/get.dart';
 import 'package:twogass/apps/controller/auth_controller.dart';
 import 'package:twogass/apps/core/constants/database.dart';
+import 'package:twogass/apps/data/model/member_model.dart';
 import 'package:twogass/apps/data/model/project_model.dart';
 import 'package:twogass/apps/data/model/task_model.dart';
 import 'package:twogass/apps/data/model/user_model.dart';
+import 'package:twogass/apps/features/organization/domain/usecase/fetch_member_org_usecase.dart';
 import 'package:twogass/apps/features/organization/presentation/controller/organization_controller.dart';
 import 'package:twogass/apps/features/project/domain/repositories/project_repository.dart';
+import 'package:twogass/apps/features/project/domain/usecase/add_assigner_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/project_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/update_task_status_usecase.dart';
+import 'package:twogass/apps/features/project/presentation/view/screen/project_assign_screen.dart';
 import 'package:yo_ui/yo_ui_base.dart';
 
 final key = DatabaseConst();
@@ -22,6 +26,7 @@ class ProjectController extends GetxController {
   final project = ProjectModel.initial().obs;
   final task = <TaskModel>[].obs;
   final taskNew = <TaskModel>[].obs;
+  final members = <MemberModel>[].obs;
   final createdBy = UserModel.initial().obs;
   final assignProject = <ProjectAssignModel>[].obs;
   final RxBool isAssigner = false.obs;
@@ -30,6 +35,8 @@ class ProjectController extends GetxController {
   UpdateTaskStatusUsecase updateTaskStatus = UpdateTaskStatusUsecase(
     Get.find(),
   );
+  FetchMemberOrgUsecase getMember = FetchMemberOrgUsecase(Get.find());
+  AddAssignerUsecase addAssign = AddAssignerUsecase(Get.find());
 
   final filtersTask = ["all", "todo", "progress", 'done'];
   final currentFilterTask = 0.obs;
@@ -73,6 +80,26 @@ class ProjectController extends GetxController {
     } finally {
       isLoading.value = false;
     }
+  }
+
+  goToAssignProject() async {
+    members.value = await getMember(orgId.value);
+    Get.back();
+    Get.to(() => ProjectAssignScreen());
+  }
+
+  addAssigner(MemberModel model) async{
+    final assignId = YoIdGenerator.alphanumericId();
+    ProjectAssignModel data = ProjectAssignModel(
+      id: assignId,
+      projectId: id.value,
+      uid: model.uid,
+      imageUrl: model.imageUrl,
+    );
+    await addAssign(data);
+    initData(
+      useLoading: true,
+    );
   }
 
   @override

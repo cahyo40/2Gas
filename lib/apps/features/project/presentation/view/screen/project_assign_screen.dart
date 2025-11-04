d58404fbcd419cc59a4c145a3b4a68cb670f75cb
cahyo40 <mdcnugroho@gmail.com>
2025-11-04 22:00:56 +0700

    feat(project-assign): add function add member

--- lib/apps/features/project/presentation/view/screen/project_assign_screen.dart
+++ lib/apps/features/project/presentation/view/screen/project_assign_screen.dart
@@ -11,31 +11,78 @@ class ProjectAssignScreen extends GetView<ProjectController> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          YoText.titleMedium("Assign"),
-          Divider(),
-          Expanded(
-            child: Obx(
-              () => ListView.builder(
-                padding: YoPadding.all16,
-                itemCount: controller.assignProject.length,
-                itemBuilder: (_, i) {
-                  final model = controller.assignProject[i];
-                  return UserListtileWidget(uid: model.uid);
-                },
-              ),
-            ),
-          ),
-          YoButton.secondary(
+    return Scaffold(
+      appBar: YoAppBar(
+        title: "Assign",
+        backgroundColor: context.backgroundColor,
+        actions: [
+          YoButtonIcon(
             icon: Icon(Iconsax.user_add_outline, color: context.colorTextBtn),
-            text: "Add Member",
-            textColor: context.colorTextBtn,
-            onPressed: () {},
+            onPressed: () {
+              YoDialog.show(
+                context: context,
+                title: "Member",
+                content: "Daftar Member Org",
+                actions: [
+                  Obx(
+                    () => ListView.builder(
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemCount: controller.members.length,
+                      itemBuilder: (_, index) {
+                        final member = controller.members[index];
+                        final bool isAssinger = controller.project.value.assign
+                            .any((e) => e.uid == member.uid);
+                        return YoListTile(
+                          leading: YoAvatar.image(imageUrl: member.imageUrl),
+                          title: member.name,
+                          subtitle: member.role.name.capitalize,
+                          onTap: () async {
+                            if (!isAssinger) {
+                              YoConfirmDialog.show(
+                                context: context,
+                                title: "title",
+                                content: "content",
+                                confirmText: "Yes",
+                              ).then((confirm) async {
+                                if (confirm == true) {
+                                  await controller.addAssigner(member);
+                                  Get.back();
+                                }
+                              });
+                            }
+                          },
+                          trailing: Visibility(
+                            visible: isAssinger,
+                            child: Icon(
+                              Iconsax.tick_square_outline,
+                              color: context.successColor,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
-          SizedBox(height: YoSpacing.lg),
+          YoSpace.widthXs(),
         ],
+      ),
+      body: SafeArea(
+        child: Obx(
+          () => controller.isLoading.isTrue
+              ? Center(child: YoLoading())
+              : ListView.builder(
+                  padding: YoPadding.all16,
+                  itemCount: controller.assignProject.length,
+                  itemBuilder: (_, i) {
+                    final model = controller.assignProject[i];
+                    return UserListtileWidget(uid: model.uid);
+                  },
+                ),
+        ),
       ),
     );
   }

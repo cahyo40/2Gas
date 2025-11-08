31a105026d1c9f83cbfd7c60088d08886481b316
cahyo40 <mdcnugroho@gmail.com>
2025-11-08 09:23:05 +0700

    feat(org-activity-screen): add filter activity

--- lib/apps/features/organization/presentation/view/screen/organizaton_activity_screen.dart
+++ lib/apps/features/organization/presentation/view/screen/organizaton_activity_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:twogass/apps/data/model/activity_model.dart';
 import 'package:twogass/apps/widget/card_activity_widget.dart';
 import 'package:yo_ui/yo_ui.dart';
 
@@ -10,18 +11,64 @@ class OrganizatonActivityScreen extends GetView<OrganizationController> {
 
   @override
   Widget build(BuildContext context) {
+    final orgColor = controller.org.value.color;
     return Scaffold(
-      body: Obx(
-        () => ListView.builder(
-          padding: YoPadding.all20,
-          shrinkWrap: true,
-          physics: AlwaysScrollableScrollPhysics(),
-          itemCount: controller.activity.length,
-          itemBuilder: (_, i) {
-            final model = controller.activity[i];
-            return CardActivityWidget(model: model);
-          },
-        ),
+      appBar: YoAppBar(
+        title: "Activity Org",
+        leading: SizedBox(),
+        backgroundColor: context.backgroundColor,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+            child: ListView.separated(
+              padding: YoPadding.symmetricH12,
+              scrollDirection: Axis.horizontal,
+              itemCount: ActivityTypeCategory.values.length,
+              separatorBuilder: (_, __) => SizedBox(width: 8),
+              itemBuilder: (context, index) {
+                final e = ActivityTypeCategory.values[index];
+
+                return Obx(
+                  () => YoChip(
+                    borderRadius: 20,
+                    backgroundColor: controller.activityFilter.value == e
+                        ? Color(orgColor ?? context.primaryColor.toARGB32())
+                        : context.backgroundColor,
+                    label: e.name.capitalize!,
+                    size: YoChipSize.large,
+                    textColor: controller.activityFilter.value == e
+                        ? context.onPrimaryColor
+                        : context.textColor,
+                    borderColor: context.gray200,
+                    onTap: () {
+                      controller.changeActivityFilter(e);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          YoSpace.heightMd(),
+          Expanded(
+            child: Obx(
+              () => controller.activityShow.isEmpty
+                  ? SizedBox(child: Center(child: YoEmptyState.noData()))
+                  : ListView.builder(
+                      padding: YoPadding.all20,
+                      shrinkWrap: true,
+                      physics: AlwaysScrollableScrollPhysics(),
+                      itemCount: controller.activityShow.length,
+                      itemBuilder: (_, i) {
+                        final model = controller.activityShow[i];
+
+                        return CardActivityWidget(model: model);
+                      },
+                    ),
+            ),
+          ),
+        ],
       ),
     );
   }

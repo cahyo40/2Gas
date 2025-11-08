0e2ef0a453f61f02723ede5110335b693d132316
cahyo40 <mdcnugroho@gmail.com>
2025-11-08 09:18:47 +0700

    feat(schedule-model): add some field and enum

--- lib/apps/data/model/schedule_model.dart
+++ lib/apps/data/model/schedule_model.dart
@@ -1,13 +1,33 @@
+// schedule_model.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+enum ScheduleType { all, deadline, event }
+
+enum ScheduleAccess { org, private }
+
+extension ScheduleTypeX on ScheduleType {
+  String get name => toString().split('.').last;
+  static ScheduleType fromName(String s) =>
+      ScheduleType.values.firstWhere((e) => e.name == s);
+}
+
+extension ScheduleAccessX on ScheduleAccess {
+  String get name => toString().split('.').last;
+  static ScheduleAccess fromName(String s) =>
+      ScheduleAccess.values.firstWhere((e) => e.name == s);
+}
 
 class ScheduleModel {
   final String id;
   final String uid;
-  final String type;
+  final ScheduleType type;
+  final ScheduleAccess access;
+  final List<String> uidAccess;
   final String? orgId;
-  final DateTime start;
-  final DateTime end;
-  final DateTime creatdAt;
+  final String? projectId;
+  final String? taskId;
+  final DateTime date;
+  final DateTime createdAt;
   final String title;
   final String? description;
 
@@ -15,25 +35,29 @@ class ScheduleModel {
     required this.id,
     required this.uid,
     required this.type,
+    required this.access,
+    required this.uidAccess,
     this.orgId,
-    required this.start,
-    required this.end,
-    required this.creatdAt,
+    this.projectId,
+    this.taskId,
+    required this.date,
+    required this.createdAt,
     required this.title,
     this.description,
   });
 
-  factory ScheduleModel.fromFirestore(DocumentSnapshot doc) =>
-      ScheduleModel.fromJson(doc.data()! as Map<String, dynamic>);
-
+  // ------------------ JSON ------------------
   factory ScheduleModel.fromJson(Map<String, dynamic> json) => ScheduleModel(
     id: json['id'] as String,
     uid: json['uid'] as String,
-    type: json['type'] as String,
+    type: ScheduleTypeX.fromName(json['type'] as String),
+    access: ScheduleAccessX.fromName(json['access'] as String),
+    uidAccess: List<String>.from(json['uidAccess'] as List),
     orgId: json['orgId'] as String?,
-    start: _dtFromJson(json['start']),
-    end: _dtFromJson(json['end']),
-    creatdAt: _dtFromJson(json['creatdAt']),
+    projectId: json['projectId'] as String?,
+    taskId: json['taskId'] as String?,
+    date: DateTime.parse(json['date'] as String),
+    createdAt: DateTime.parse(json['createdAt'] as String),
     title: json['title'] as String,
     description: json['description'] as String?,
   );
@@ -41,95 +65,58 @@ class ScheduleModel {
   Map<String, dynamic> toJson() => {
     'id': id,
     'uid': uid,
-    'type': type,
+    'type': type.name,
+    'access': access.name,
+    'uidAccess': uidAccess,
     'orgId': orgId,
-    'start': _dtToJson(start),
-    'end': _dtToJson(end),
-    'creatdAt': _dtToJson(creatdAt),
+    'projectId': projectId,
+    'taskId': taskId,
+    'date': date.toIso8601String(),
+    'createdAt': createdAt.toIso8601String(),
     'title': title,
     'description': description,
   };
 
-  factory ScheduleModel.fromMap(Map<String, dynamic> map) => ScheduleModel(
-    id: map['id'] as String,
-    uid: map['uid'] as String,
-    type: map['type'] as String,
-    orgId: map['orgId'] as String?,
-    start: DateTime.fromMillisecondsSinceEpoch(map['start'] as int),
-    end: DateTime.fromMillisecondsSinceEpoch(map['end'] as int),
-    creatdAt: DateTime.fromMillisecondsSinceEpoch(map['creatdAt'] as int),
-    title: map['title'] as String,
-    description: map['description'] as String?,
-  );
+  // ------------------ Sembast / Map ------------------
+  factory ScheduleModel.fromMap(Map<String, dynamic> map) =>
+      ScheduleModel.fromJson(map);
 
-  Map<String, dynamic> toMap() => {
-    'id': id,
-    'uid': uid,
-    'type': type,
-    'orgId': orgId,
-    'start': start.millisecondsSinceEpoch,
-    'end': end.millisecondsSinceEpoch,
-    'creatdAt': creatdAt.millisecondsSinceEpoch,
-    'title': title,
-    'description': description,
-  };
+  Map<String, dynamic> toMap() => toJson();
 
-  static int _dtToJson(DateTime dt) => dt.millisecondsSinceEpoch;
-  static DateTime _dtFromJson(dynamic json) => json is int
-      ? DateTime.fromMillisecondsSinceEpoch(json)
-      : (json as Timestamp).toDate();
+  // ------------------ Firestore ------------------
+  factory ScheduleModel.fromFirestore(DocumentSnapshot doc) =>
+      ScheduleModel.fromJson(
+        doc.data()! as Map<String, dynamic>,
+      ).copyWith(id: doc.id);
 
+  Map<String, dynamic> toFirestore() => toJson()..remove('id');
+
+  // ------------------ Copy ------------------
   ScheduleModel copyWith({
     String? id,
     String? uid,
-    String? type,
+    ScheduleType? type,
+    ScheduleAccess? access,
+    List<String>? uidAccess,
     String? orgId,
-    DateTime? start,
-    DateTime? end,
-    DateTime? creatdAt,
+    String? projectId,
+    String? taskId,
+    DateTime? date,
+    DateTime? createdAt,
     String? title,
     String? description,
   }) => ScheduleModel(
     id: id ?? this.id,
     uid: uid ?? this.uid,
     type: type ?? this.type,
+    access: access ?? this.access,
+    uidAccess: uidAccess ?? this.uidAccess,
     orgId: orgId ?? this.orgId,
-    start: start ?? this.start,
-    end: end ?? this.end,
-    creatdAt: creatdAt ?? this.creatdAt,
+    projectId: projectId ?? this.projectId,
+    taskId: taskId ?? this.taskId,
+    date: date ?? this.date,
+    createdAt: createdAt ?? this.createdAt,
     title: title ?? this.title,
     description: description ?? this.description,
   );
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is ScheduleModel &&
-          runtimeType == other.runtimeType &&
-          id == other.id &&
-          uid == other.uid &&
-          type == other.type &&
-          orgId == other.orgId &&
-          start == other.start &&
-          end == other.end &&
-          creatdAt == other.creatdAt &&
-          title == other.title &&
-          description == other.description;
-
-  @override
-  int get hashCode => Object.hash(
-    id,
-    uid,
-    type,
-    orgId,
-    start,
-    end,
-    creatdAt,
-    title,
-    description,
-  );
-
-  @override
-  String toString() =>
-      'ScheduleModel(id: $id, uid: $uid, type: $type, title: $title)';
 }

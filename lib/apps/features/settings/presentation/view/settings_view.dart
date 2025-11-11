ebf4fb1bb470e2366fd99f52ab9e4820ad88d1d3
cahyo40 <mdcnugroho@gmail.com>
2025-11-11 10:09:27 +0700

    feat(settings): add change theme, change language and change style logout

--- lib/apps/features/settings/presentation/view/settings_view.dart
+++ lib/apps/features/settings/presentation/view/settings_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:twogass/apps/controller/auth_controller.dart';
+import 'package:twogass/apps/controller/locale_controller.dart';
+import 'package:twogass/apps/controller/theme_controller.dart';
+import 'package:twogass/apps/core/helpers/localization.dart';
 import 'package:yo_ui/yo_ui_base.dart';
 
 import '../controller/settings_controller.dart';
@@ -11,19 +15,51 @@ class SettingsView extends GetView<SettingsController> {
   @override
   Widget build(BuildContext context) {
     final auth = Get.find<AuthController>();
+    final theme = Get.find<ThemeController>();
+    final locale = Get.find<LocaleController>();
     return Scaffold(
       appBar: AppBar(title: Text('SettingsView'.tr), centerTitle: true),
       body: SafeArea(
         child: Column(
           children: [
-            YoButton.ghost(
-              text: "sign out",
-              onPressed: () async {
-                YoConfirmDialog.show(
+            Obx(
+              () => YoListTile(
+                title: L10n.t.theme,
+                trailing: YoText.bodyMedium(
+                  theme.isDarkMode.isTrue
+                      ? L10n.t.dark_mode
+                      : L10n.t.light_mode,
+                ),
+                onTap: () {
+                  theme.toggle();
+                },
+              ),
+            ),
+            Obx(
+              () => YoListTile(
+                title: L10n.t.language,
+
+                trailing: YoText.bodyMedium(
+                  locale.isIndonesia ? "Indonesia" : "English",
+                ),
+                onTap: () {
+                  locale.toggle();
+                },
+              ),
+            ),
+            Spacer(),
+            YoListTile(
+              title: L10n.t.logout,
+              leading: Icon(Iconsax.logout_outline),
+              onTap: () {
+                YoAdvancedConfirmDialog.show(
                   context: context,
-                  title: "Logout",
-                  content: "oke gan",
-                  confirmText: "Gas",
+                  title: L10n.t.msg_logout_title,
+                  content: L10n.t.msg_logout_content,
+                  confirmText: L10n.t.accept,
+                  cancelText: L10n.t.decline,
+                  confirmVariant: YoButtonVariant.primary,
+                  cancelVariant: YoButtonVariant.outline,
                 ).then((confirm) async {
                   if (confirm == true) {
                     await auth.signOut();

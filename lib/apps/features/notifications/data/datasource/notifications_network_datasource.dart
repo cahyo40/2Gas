cd0049289258e3a6c528c7f119f9875693902fba
cahyo40 <mdcnugroho@gmail.com>
2025-11-15 18:33:26 +0700

    notification: function to getNotification

--- lib/apps/features/notifications/data/datasource/notifications_network_datasource.dart
+++ lib/apps/features/notifications/data/datasource/notifications_network_datasource.dart
@@ -1,3 +1,26 @@
-class NotificationsNetworkDatasource {
-  // TODO: remote api calls
+import 'package:get/get.dart';
+import 'package:twogass/apps/controller/auth_controller.dart';
+import 'package:twogass/apps/core/services/firebase.dart';
+import 'package:twogass/apps/data/model/notifications_model.dart';
+import 'package:twogass/apps/features/notifications/domain/repositories/notifications_repository.dart';
+import 'package:yo_ui/yo_ui_base.dart';
+
+class NotificationsNetworkDatasource implements NotificationsRepository {
+  String get uid => Get.find<AuthController>().uid;
+  @override
+  Future<List<NotificationsModel>> getNotification() async {
+    try {
+      final res = await FirebaseServices.notif
+          .where("uidShows", arrayContains: uid)
+          .orderBy("createdAt", descending: true)
+          .get();
+      final data = res.docs
+          .map((doc) => NotificationsModel.fromFirestore(doc))
+          .toList();
+      return data;
+    } catch (e, s) {
+      YoLogger.error("Get Schedule => $e -> $s");
+      return [];
+    }
+  }
 }
--- lib/apps/features/notifications/data/repositories/notifications_repository_impl.dart
+++ lib/apps/features/notifications/data/repositories/notifications_repository_impl.dart
@@ -1,10 +1,15 @@
+import 'package:twogass/apps/data/model/notifications_model.dart';
+
 import '../../domain/repositories/notifications_repository.dart';
 import '../datasource/notifications_network_datasource.dart';
-import '../datasource/notifications_offline_datasource.dart';
 
 class NotificationsRepositoryImpl implements NotificationsRepository {
   final NotificationsNetworkDatasource _network;
-  final NotificationsOfflineDatasource _offline;
 
-  NotificationsRepositoryImpl(this._network, this._offline);
+  NotificationsRepositoryImpl(this._network);
+
+  @override
+  Future<List<NotificationsModel>> getNotification() async {
+    return await _network.getNotification();
+  }
 }

c1334e6ac47db1e8452ef77e386440067d49b784
cahyo40 <mdcnugroho@gmail.com>
2025-11-17 12:46:38 +0700

    feat(org): add widget at settings - widget for delete organization - widget avatar for icon

--- lib/apps/features/organization/presentation/view/screen/organizaton_settings_screen.dart
+++ lib/apps/features/organization/presentation/view/screen/organizaton_settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:twogass/apps/core/helpers/localization.dart';
+import 'package:twogass/apps/data/model/member_model.dart';
 import 'package:twogass/apps/features/organization/presentation/view/screen/org_settings_member_screen.dart';
 import 'package:yo_ui/yo_ui.dart';
 
@@ -20,7 +21,10 @@ class OrganizatonSettingsScreen extends GetView<OrganizationController> {
             Obx(
               () => YoListTile(
                 title: L10n.t.member,
-                leading: Icon(Iconsax.profile_2user_outline),
+                leading: YoAvatar.icon(
+                  icon: Iconsax.profile_2user_outline,
+                  backgroundColor: context.cardColor,
+                ),
                 onTap: () {
                   controller.membersFilter();
                   Get.to(() => OrgSettingsMemberScreen());
@@ -35,10 +39,40 @@ class OrganizatonSettingsScreen extends GetView<OrganizationController> {
                 ),
               ),
             ),
+
+            Visibility(
+              visible: controller.myRole.value == MemberRole.owner,
+              child: YoListTile(
+                title: L10n.t.delete_project,
+                leading: YoAvatar.icon(
+                  icon: Iconsax.trash_outline,
+                  backgroundColor: context.errorColor,
+                  iconColor: context.onPrimaryColor,
+                ),
+                onTap: () {
+                  YoAdvancedConfirmDialog.show(
+                    context: context,
+                    title: L10n.t.dialog_delete_org_title,
+                    content: L10n.t.dialog_delete_org_content,
+                    confirmText: L10n.t.yes,
+                    confirmVariant: YoButtonVariant.custom,
+                    confirmColor: context.primaryColor,
+                    cancelText: L10n.t.no,
+                  ).then((confirm) {
+                    if (confirm == true && context.mounted) {
+                      controller.onDeleteOrg();
+                    }
+                  });
+                },
+              ),
+            ),
             Spacer(),
             ListTile(
               title: YoText(L10n.t.back_to_home),
-              leading: Icon(Iconsax.home_1_outline),
+              leading: YoAvatar.icon(
+                icon: Iconsax.home_1_outline,
+                backgroundColor: context.cardColor,
+              ),
               onTap: () => Get.back(),
             ),
           ],

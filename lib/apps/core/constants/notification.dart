3ede23e21a123118fdf22a2d2c7264ed4146eda9
cahyo40 <mdcnugroho@gmail.com>
2025-11-20 08:06:41 +0700

    feat)notif): change apiKey

--- lib/apps/core/constants/notification.dart
+++ lib/apps/core/constants/notification.dart
@@ -1,5 +1,5 @@
 class NotificationsConst {
   static String appId = "648cb1cb-da83-4ea0-b31f-4cba69f7fc31";
-  static String apiKey =
-      "os_v2_app_msglds62qnhkbmy7js5gt574ghuyw2jjyxkerv4zta4qocrf5xhfznrpcezcjywpoywfaiupvsqhtockpbc5vbdzejufsef2tljwzoa";
+  static String restApiKey =
+      "os_v2_app_msglds62qnhkbmy7js5gt574gfn4pucpkp4eaq5rixm4rp5yosqdkb54qnrwac7urjlmslc7kva4gcwkrorsjsndmbncyd5rqnkocsy";
 }

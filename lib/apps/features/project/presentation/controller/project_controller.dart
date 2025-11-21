d1f5be96efd7fad369832b5555a2646927a225c1
cahyo40 <mdcnugroho@gmail.com>
2025-11-21 18:55:45 +0700

    feat(project-con): add method - onAddComment - onTaskFilter

--- lib/apps/features/project/presentation/controller/project_controller.dart
+++ lib/apps/features/project/presentation/controller/project_controller.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:twogass/apps/controller/auth_controller.dart';
 import 'package:twogass/apps/core/constants/database.dart';
 import 'package:twogass/apps/core/helpers/localization.dart';
+import 'package:twogass/apps/data/model/comment_model.dart';
 import 'package:twogass/apps/data/model/member_model.dart';
 import 'package:twogass/apps/data/model/project_model.dart';
 import 'package:twogass/apps/data/model/task_model.dart';
@@ -10,8 +12,10 @@ import 'package:twogass/apps/features/organization/domain/usecase/fetch_member_o
 import 'package:twogass/apps/features/organization/presentation/controller/organization_controller.dart';
 import 'package:twogass/apps/features/project/domain/repositories/project_repository.dart';
 import 'package:twogass/apps/features/project/domain/usecase/add_assigner_usecase.dart';
+import 'package:twogass/apps/features/project/domain/usecase/add_comment_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/delete_project_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/delete_task_usecase.dart';
+import 'package:twogass/apps/features/project/domain/usecase/fetch_comment_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/project_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/update_project_usecase.dart';
 import 'package:twogass/apps/features/project/domain/usecase/update_task_status_usecase.dart';
@@ -29,11 +33,16 @@ class ProjectController extends GetxController {
   final orgId = "".obs;
   final project = ProjectModel.initial().obs;
   final task = <TaskModel>[].obs;
+  final taskFilter = <TaskModel>[].obs;
   final taskNew = <TaskModel>[].obs;
   final members = <MemberModel>[].obs;
+  final comments = <CommentModel>[].obs;
   final createdBy = UserModel.initial().obs;
   final assignProject = <ProjectAssignModel>[].obs;
   final RxBool isAssigner = false.obs;
+
+  final commentCtrl = TextEditingController();
+  final form = GlobalKey<FormState>();
 
   ProjectUsecase projectUsecase = ProjectUsecase(Get.find<ProjectRepository>());
   UpdateTaskStatusUsecase updateTaskStatus = UpdateTaskStatusUsecase(
@@ -43,10 +52,26 @@ class ProjectController extends GetxController {
   AddAssignerUsecase addAssign = AddAssignerUsecase(Get.find());
   UpdateProjectUsecase updateProject = UpdateProjectUsecase(Get.find());
   DeleteTaskUsecase deleteTask = DeleteTaskUsecase(Get.find());
+  AddCommentUsecase addComment = AddCommentUsecase(Get.find());
+  FetchCommentUsecase fetchComment = FetchCommentUsecase(Get.find());
   DeleteProjectUsecase deleteProject = DeleteProjectUsecase(Get.find());
 
   final filtersTask = ["all", "todo", "progress", 'done'];
   final currentFilterTask = 0.obs;
+
+  onTaskFilter(int index) {
+    currentFilterTask.value = index;
+    if (currentFilterTask.value == 0) {
+      taskFilter.value = taskNew;
+    } else {
+      taskFilter.value = taskNew
+          .where(
+            (task) => task.status == TaskStatus.values[currentFilterTask.value],
+          )
+          .toList();
+    }
+    taskFilter.refresh();
+  }
 
   updateStatusTask(String taskId, String projectId, TaskStatus status) async {
     await updateTaskStatus.call(
@@ -54,7 +79,7 @@ class ProjectController extends GetxController {
       status: status,
       projectId: projectId,
     );
-    // await initData(useLoading: false);
+    await initData(useLoading: false);
   }
 
   refreshTask() async {
@@ -65,14 +90,16 @@ class ProjectController extends GetxController {
     isLoading.value = useLoading;
     try {
       final data = await projectUsecase(id.value, orgId.value);
+      comments.value = await fetchComment(id.value);
       project.value = data[key.project];
       task.value = data[key.task];
       taskNew.value = task.where((e) => e.status != TaskStatus.done).toList()
         ..sort((a, b) => (a.deadline).compareTo(b.deadline));
-
+      taskFilter.value = taskNew;
       assignProject.value = data[key.projectAssign];
       createdBy.value = data["createdBy"];
-
+      final allMember = await getMember(orgId.value);
+      members.value = allMember.where((e) => e.isPending == false).toList();
       isAssigner.value = assignProject
           .where((e) => e.uid == user.uid)
           .isNotEmpty;
@@ -83,8 +110,9 @@ class ProjectController extends GetxController {
   }
 
   goToAssignProject() async {
-    final allMember = await getMember(orgId.value);
-    members.value = allMember.where((e) => e.isPending == false).toList();
+    currentFilterTask.value = 0;
+    taskFilter.value = taskNew;
+    taskFilter.refresh();
     Get.back();
     Get.to(() => ProjectAssignScreen());
   }
@@ -143,6 +171,28 @@ class ProjectController extends GetxController {
   onDeleteProject() async {
     await deleteProject(project.value);
     Get.back();
+  }
+
+  onAddComment() async {
+    try {
+      if (form.currentState!.validate()) {
+        final commentId = YoIdGenerator.alphanumericId();
+        final data = CommentModel(
+          id: commentId,
+          orgId: orgId.value,
+          projectId: id.value,
+          createdBy: user.uid,
+          comment: commentCtrl.text.trim(),
+          createdAt: DateTime.now(),
+        );
+
+        await addComment(data);
+      }
+    } catch (_) {
+    } finally {
+      comments.value = await fetchComment(id.value);
+      commentCtrl.clear();
+    }
   }
 
   @override
